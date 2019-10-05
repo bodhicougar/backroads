43f4b23a7a3a18089555877600198c56851ae917
Walter O. Achammer <contact@blackboxmd.fyi>
2019-10-05 19:20:27 +0200

    Navbar aufgesetzt

---
+++ src/constants/social-icons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaFacebook, FaTwitterSquare, FaInstagram } from 'react-icons/fa'
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: 'https://www.facebook.com/bbmediadesign/',
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: 'https://twitter.com/BBMediaDesign',
+  },
+  {
+    icon: <FaInstagram />,
+    url: 'https://www.instagram.com/blackboxmediadesign/',
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import { FaAdobe } from 'react-icons/fa'
 
 export default () => (
   <Layout>
     <div>Hello from Backroad Tours</div>
+    <FaAdobe />
   </Layout>
 )
