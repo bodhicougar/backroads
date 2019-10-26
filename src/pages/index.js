0ec36fad81ff6b151fa2665f681ffca88a6a26d1
Walter O. Achammer <contact@blackboxmd.fyi>
2019-10-26 21:09:12 +0200

    Banner eingebaut

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,19 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
 
 export default () => (
   <Layout>
-    <SimpleHero />
+    <SimpleHero>
+      <Banner
+        title="Continue exploring"
+        info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+      >
+        <Link to="/tours" className="btn-white">
+          Explore our tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
