7937bea2fe068cb06239c2082db8d91f7a87f12d
Walter O. Achammer <contact@blackboxmd.fyi>
2019-10-28 11:54:24 +0100

    Error Seiten neu aufgebaut

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../css/error.module.css'
+import { Link } from 'gatsby'
+import Banner from '../components/Banner'
 
 export default function error() {
-  return <Layout>Ein Fehler ist aufgetreten, Du Haubentaucher!</Layout>
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="Einbahnstrasse: Ein Fehler ist aufgetreten!">
+          <Link to="/" className="btn-white">
+            Zurück zur Startseite
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
