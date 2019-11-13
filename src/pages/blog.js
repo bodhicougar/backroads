3f309998425a4fa1e86677853579ee522ff4f62c
bodhicougar <info@blackboxmd.fyi>
2019-11-13 22:43:23 +0100

    Mit images herumspielen

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Images from '../examples/Images'
 
 const blog = () => {
   return (
     <Layout>
       Hello from blog page: <Link to="/">Back home</Link>
+      <Images />
     </Layout>
   )
 }
