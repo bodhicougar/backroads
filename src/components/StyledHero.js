fbcb1092ac6a48de24ec575a2d541639cbb520a9
bodhicougar <info@blackboxmd.fyi>
2019-11-14 21:18:34 +0100

    ImageFluid eingebaut

---
+++ src/components/StyledHero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
+
+const StyledHero = ({ img, className, children, home }) => {
+  return <div></div>
+}
+
+export default StyledHero
