74e07460acaf85df3dab0327f35f6c0ab22583ef
bodhicougar <info@blackboxmd.fyi>
2019-11-18 10:58:13 +0100

    Hintergrundbilder Automatik

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
-import SimpleHero from '../components/SimpleHero'
+import StyledHero from '../components/StyledHero'
 import Banner from '../components/Banner'
 import About from '../components/Home/About'
 import Services from '../components/Home/Services'
+import { graphql, StaticQuery } from 'gatsby'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <SimpleHero>
+    <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
       <Banner
         title="Continue exploring"
         info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
@@ -17,8 +18,20 @@ export default () => (
           Explore our tours
         </Link>
       </Banner>
-    </SimpleHero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
